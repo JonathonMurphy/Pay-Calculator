979ce5e967d13a82a65def7fe1c3f511f7d08eaa
JonathonMurphy <JSMurphy1991@gmail.com>
2018-04-25 17:27:22 -0400

    Added new functions to hourlyToSalary.js

--- hourlyToSalary.js
+++ hourlyToSalary.js
@@ -1,26 +1,55 @@
-var hourlyRate = 9.25;
-var salaryRate = 37000;
-var hoursPerWeek =30;
+var hoursPerWeek =40;
 var weeksPerYear = 52;
 var hoursPerYear;
+
+var hourlyRate = 16;
+var salaryRate = 38000;
+
 var hourlyToSalaryRate;
 var salaryToHourlyRate;
+var hourlyPerWeek;
+var salaryPerWeek;
+var hourlyPerMonth;
+var salaryPerMonth;
 
 function totalHours(hoursPerWeek, weeksPerYear){
   hoursPerYear = hoursPerWeek * weeksPerYear;
   return hoursPerYear;
 }
+totalHours(hoursPerWeek, weeksPerYear);
 
 function hourlyToSalary(hoursPerYear, hourlyRate){
   hourlyToSalaryRate = hoursPerYear * hourlyRate;
   return hourlyToSalaryRate;
 }
+hourlyToSalary(hoursPerYear, hourlyRate);
 
 function salaryToHourly(hoursPerYear, salaryRate){
   salaryToHourlyRate = salaryRate / hoursPerYear;
   return salaryToHourlyRate;
 }
-
-totalHours(hoursPerWeek, weeksPerYear);
-hourlyToSalary(hoursPerYear, hourlyRate);
 salaryToHourly(hoursPerYear, salaryRate);
+
+function hourlyToWeekly(hourlyRate, hoursPerWeek){
+  hourlyPerWeek = hourlyRate * hoursPerWeek;
+  return hourlyPerWeek;
+}
+hourlyToWeekly(hourlyRate, hoursPerWeek);
+
+function salaryToWeekly(salaryRate, weeksPerYear){
+  salaryPerWeek = salaryRate / weeksPerYear;
+  return salaryPerWeek;
+}
+salaryToWeekly(salaryRate, weeksPerYear);
+
+function hourlyToMonthly(hourlyRate, weeksPerYear){
+  hourlyPerMonth = hourlyRate * (hoursPerWeek * (weeksPerYear/12));
+  return hourlyPerMonth;
+}
+hourlyToMonthly(hourlyRate, weeksPerYear);
+
+function salarytoMonthly(salaryRate, weeksPerYear){
+  salaryPerMonth = salaryRate / 12;
+  return salaryPerMonth;
+}
+salarytoMonthly(salaryRate, weeksPerYear);
