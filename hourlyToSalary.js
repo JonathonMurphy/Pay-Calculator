cdb02aa54c149e7663a65311e218ef6f92118f4c
JonathonMurphy <JSMurphy1991@gmail.com>
2018-04-26 23:02:26 -0400

    add toFixed() to functions where nessecary

--- hourlyToSalary.js
+++ hourlyToSalary.js
@@ -2,8 +2,8 @@ var hoursPerWeek =40;
 var weeksPerYear = 52;
 var hoursPerYear;
 
-var hourlyRate = 16;
-var salaryRate = 38000;
+var hourlyRate = 16.25;
+var salaryRate = 45000;
 
 var hourlyToSalaryRate;
 var salaryToHourlyRate;
@@ -12,44 +12,58 @@ var salaryPerWeek;
 var hourlyPerMonth;
 var salaryPerMonth;
 
+//Calculates the total hours worked per year
 function totalHours(hoursPerWeek, weeksPerYear){
   hoursPerYear = hoursPerWeek * weeksPerYear;
   return hoursPerYear;
 }
 totalHours(hoursPerWeek, weeksPerYear);
 
+//Converts an hourly pay rate into a salary pay rate
 function hourlyToSalary(hoursPerYear, hourlyRate){
   hourlyToSalaryRate = hoursPerYear * hourlyRate;
   return hourlyToSalaryRate;
 }
 hourlyToSalary(hoursPerYear, hourlyRate);
 
+//Converts a salary pay rate into an hourly pay rate
 function salaryToHourly(hoursPerYear, salaryRate){
-  salaryToHourlyRate = salaryRate / hoursPerYear;
+  var salaryToHourlyRateUnrounded = salaryRate / hoursPerYear;
+  salaryToHourlyRate = salaryToHourlyRateUnrounded.toFixed(2);
   return salaryToHourlyRate;
 }
 salaryToHourly(hoursPerYear, salaryRate);
 
+//Shows the pre-tax earnings per week at an hourly rate
 function hourlyToWeekly(hourlyRate, hoursPerWeek){
-  hourlyPerWeek = hourlyRate * hoursPerWeek;
+  hourlyPerWeekUnrounded = hourlyRate * hoursPerWeek;
+  hourlyPerWeek = hourlyPerWeekUnrounded.toFixed(2);
   return hourlyPerWeek;
 }
 hourlyToWeekly(hourlyRate, hoursPerWeek);
 
+//Shows the pre-tax earnings per week at a salary rate
 function salaryToWeekly(salaryRate, weeksPerYear){
-  salaryPerWeek = salaryRate / weeksPerYear;
+  var salaryPerWeekUnrounded = salaryRate / weeksPerYear;
+  salaryPerWeek = salaryPerWeekUnrounded.toFixed(2);
   return salaryPerWeek;
 }
 salaryToWeekly(salaryRate, weeksPerYear);
 
+//Shows the pre-tax earnings per month at an hourly rate
+//4.333 week month
 function hourlyToMonthly(hourlyRate, weeksPerYear){
-  hourlyPerMonth = hourlyRate * (hoursPerWeek * (weeksPerYear/12));
+  var hourlyPerMonthUnrounded = hourlyRate * (hoursPerWeek * (weeksPerYear/12));
+  hourlyPerMonth = hourlyPerMonthUnrounded.toFixed(2);
   return hourlyPerMonth;
 }
 hourlyToMonthly(hourlyRate, weeksPerYear);
 
+//Shows the pre-tax earnings per month at a salary rate
+//4.333 week month
 function salarytoMonthly(salaryRate, weeksPerYear){
-  salaryPerMonth = salaryRate / 12;
+  salaryPerMonthUnrounded = salaryRate / 12;
+  salaryPerMonth = salaryPerMonthUnrounded.toFixed(2);
   return salaryPerMonth;
 }
 salarytoMonthly(salaryRate, weeksPerYear);
