c5ff98a170ab6c717c05450f5edb11b185194bf2
JonathonMurphy <JSMurphy1991@gmail.com>
2018-05-16 14:37:47 -0400

    completed basic functionality

--- js/hourlyToSalary.js
+++ js/hourlyToSalary.js
@@ -1,82 +1,161 @@
+//Measure height of outer-container
+var $outerContainerHeight = $('.outer-container').height();
+//Measure height of viewport
+var $windowHeight = $(window).height();
+//Declare variable
+var setPaddingBottom;
+//Set padding-botton of outer-container to height of viewport minus 20px
+$('.outer-container').css("padding-bottom", function(setPaddingBottom) {
+  setPaddingBottom = $windowHeight - $outerContainerHeight - 20;
+  return setPaddingBottom;
+});
+
+
 var hoursPerWeek =40;
 var weeksPerYear = 52;
-var hoursPerYear;
+var hoursPerYear = 2080;
 
-var hourlyRate = 16.25;
-var salaryRate = 45000;
-
-var hourlyToSalaryRate;
-var salaryToHourlyRate;
+var hourlyPerHour;
 var hourlyPerWeek;
-var salaryPerWeek;
 var hourlyPerMonth;
+var hourlyToSalaryRate;
+
+var salaryToHourlyRate;
+var salaryPerWeek;
 var salaryPerMonth;
+var salaryPerYear;
+
+// HOURLY FUNCTIONS
+
+//Shows the pre-tax earnings per week at an hourly rate
+function hourlyToWeekly($payRateVal, hoursPerWeek){
+  hourlyPerWeekUnrounded = $payRateVal * hoursPerWeek;
+  hourlyPerWeek = hourlyPerWeekUnrounded.toFixed(2);
+  return hourlyPerWeek;
+}
+//Shows the pre-tax earnings per month at an hourly rate
+//4.333 week month
+function hourlyToMonthly($payRateVal, weeksPerYear){
+  var hourlyPerMonthUnrounded = $payRateVal * (hoursPerWeek * (weeksPerYear/12));
+  hourlyPerMonth = hourlyPerMonthUnrounded.toFixed(2);
+  return hourlyPerMonth;
+}
+//Converts an hourly pay rate into a salary pay rate
+function hourlyToSalary(hoursPerYear, $payRateVal){
+  hourlyToSalaryRate = hoursPerYear * $payRateVal;
+  return hourlyToSalaryRate;
+}
+
+//SALARY FUNCTIONS
+
+//Converts a salary pay rate into an hourly pay rate
+function salaryToHourly(hoursPerYear, $payRateVal){
+  var salaryToHourlyRateUnrounded = $payRateVal / hoursPerYear;
+  salaryToHourlyRate = salaryToHourlyRateUnrounded.toFixed(2);
+  return salaryToHourlyRate;
+}
+//Shows the pre-tax earnings per week at a salary rate
+function salaryToWeekly($payRateVal, weeksPerYear){
+  var salaryPerWeekUnrounded = $payRateVal / weeksPerYear;
+  salaryPerWeek = salaryPerWeekUnrounded.toFixed(2);
+  return salaryPerWeek;
+}
+//Shows the pre-tax earnings per month at a salary rate
+//4.333 week month
+function salarytoMonthly($payRateVal, weeksPerYear){
+  salaryPerMonthUnrounded = $payRateVal / 12;
+  salaryPerMonth = salaryPerMonthUnrounded.toFixed(2);
+  return salaryPerMonth;
+}
+
 
 //Radio button selection
 //Hourly Rate Input Form
 //Salary Rate Input Form
 
+//Pay rate
+var $payRate = $(':input[type="number"]');
+//Radio buttons
+// var $radioButton = $('form:radio');
 //Per Hour
-var perHourHTML = document.getElementById('perHour');
+var $perHour = $('#perHour');
 //Per Week
-var perWeekHTML = document.getElementById('perWeek');
+var $perWeek = $('#perWeek');
 //Per Month
-var perMonthHTML = document.getElementById('perMonth');
+var $perMonth = $('#perMonth');
 //Per Year
-var perYearHTML = document.getElementById('perYear');
+var $perYear = $('#perYear');
+//Submit button
+var $submit = $('#submit')
+//Submit button click action
+$submit.on('click', function(e) {
+  e.preventDefault();
+  //Clear results
+  $perHour.text('');
+  $perWeek.text('');
+  $perMonth.text('');
+  $perYear.text('');
+  //Get number from Form
+  var $payRateVal = $payRate.val();
+  //Check which radio button is checked
+  if ($('.radio[value=hourly]').is(':checked')) { //Hourly
+    //Run number through per hour function
+    //Update per hour
+    $perHour.append("Per Hour: " + $payRateVal);
+    //Run number through per week function
+    hourlyToWeekly($payRateVal, hoursPerWeek);
+    //Update per week
+    $perWeek.append("Per Week: " + hourlyPerWeek);
+    //Run number through per month function
+    hourlyToMonthly($payRateVal, weeksPerYear);
+    //Update per month
+    $perMonth.append("Per Month: " + hourlyPerMonth);
+    //Run number through per year function
+    hourlyToSalary(hoursPerYear, $payRateVal);
+    //Update per year
+    $perYear.append("Per Year: " + hourlyToSalaryRate);
+    //Measure height of outer-container
+    var $outerContainerHeight = $('.outer-container').height();
+    //Measure height of viewport
+    var $windowHeight = $(window).height();
+    //Declare variable
+    var setPaddingBottom;
+    //Clear oadding-bottom
+    $('.outer-container').css("padding-bottom", "0")
+    //Set padding-bottom
+    $('.outer-container').css("padding-bottom", function(setPaddingBottom) {
+      setPaddingBottom = $windowHeight - $outerContainerHeight - 20;
+      return setPaddingBottom;
+    });
+  } else { //Salary
+    //Run number through per hour function
+    salaryToHourly(hoursPerYear, $payRateVal);
+    //Update per hour
+    $perHour.append("Per Hour: " + salaryToHourlyRate);
+    //Run number through per week function
+    salaryToWeekly($payRateVal, weeksPerYear);
+    //Update per week
+    $perWeek.append("Per Week: " + salaryPerWeek);
+    //Run number through per month function
+    salarytoMonthly($payRateVal, weeksPerYear);
+    //Update per month
+    $perMonth.append("Per Month: " + salaryPerMonth);
+    //Run number through per year function
+    //Update per year
+    $perYear.append("Per Year: " + $payRateVal);
+    //Measure height of outer-container
+    var $outerContainerHeight = $('.outer-container').height();
+    //Measure height of viewport
+    var $windowHeight = $(window).height();
+    //Declare variable
+    var setPaddingBottom;
+    //Clear oadding-bottom
+    $('.outer-container').css("padding-bottom", "0")
+    //Set padding-bottom
+    $('.outer-container').css("padding-bottom", function(setPaddingBottom) {
+      setPaddingBottom = $windowHeight - $outerContainerHeight - 20;
+      return setPaddingBottom;
+    });
+  }
 
-//Calculates the total hours worked per year
-function totalHours(hoursPerWeek, weeksPerYear){
-  hoursPerYear = hoursPerWeek * weeksPerYear;
-  return hoursPerYear;
-}
-totalHours(hoursPerWeek, weeksPerYear);
-
-//Converts an hourly pay rate into a salary pay rate
-function hourlyToSalary(hoursPerYear, hourlyRate){
-  hourlyToSalaryRate = hoursPerYear * hourlyRate;
-  return hourlyToSalaryRate;
-}
-hourlyToSalary(hoursPerYear, hourlyRate);
-
-//Converts a salary pay rate into an hourly pay rate
-function salaryToHourly(hoursPerYear, salaryRate){
-  var salaryToHourlyRateUnrounded = salaryRate / hoursPerYear;
-  salaryToHourlyRate = salaryToHourlyRateUnrounded.toFixed(2);
-  return salaryToHourlyRate;
-}
-salaryToHourly(hoursPerYear, salaryRate);
-
-//Shows the pre-tax earnings per week at an hourly rate
-function hourlyToWeekly(hourlyRate, hoursPerWeek){
-  hourlyPerWeekUnrounded = hourlyRate * hoursPerWeek;
-  hourlyPerWeek = hourlyPerWeekUnrounded.toFixed(2);
-  return hourlyPerWeek;
-}
-hourlyToWeekly(hourlyRate, hoursPerWeek);
-
-//Shows the pre-tax earnings per week at a salary rate
-function salaryToWeekly(salaryRate, weeksPerYear){
-  var salaryPerWeekUnrounded = salaryRate / weeksPerYear;
-  salaryPerWeek = salaryPerWeekUnrounded.toFixed(2);
-  return salaryPerWeek;
-}
-salaryToWeekly(salaryRate, weeksPerYear);
-
-//Shows the pre-tax earnings per month at an hourly rate
-//4.333 week month
-function hourlyToMonthly(hourlyRate, weeksPerYear){
-  var hourlyPerMonthUnrounded = hourlyRate * (hoursPerWeek * (weeksPerYear/12));
-  hourlyPerMonth = hourlyPerMonthUnrounded.toFixed(2);
-  return hourlyPerMonth;
-}
-hourlyToMonthly(hourlyRate, weeksPerYear);
-
-//Shows the pre-tax earnings per month at a salary rate
-//4.333 week month
-function salarytoMonthly(salaryRate, weeksPerYear){
-  salaryPerMonthUnrounded = salaryRate / 12;
-  salaryPerMonth = salaryPerMonthUnrounded.toFixed(2);
-  return salaryPerMonth;
-}
-salarytoMonthly(salaryRate, weeksPerYear);
+});
